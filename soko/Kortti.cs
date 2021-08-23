6a6180c8ae3b13b68f26b077ae0681aabb7f2a2e
Saarela Eero Artturi Tapio <eero.saarela6@gmail.com>
2021-08-23 22:21:55 +0300

    guihin lisätty kortti luokka

---
+++ soko/Kortti.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace soko
+{
+	public enum Arvo
+	{
+		Assa = 1,
+		Kakkonen,
+		Kolmonen,
+		Nelonen,
+		Vitonen,
+		Kutonen,
+		Seiska,
+		Kasi,
+		Ysi,
+		Kymppi,
+		Jatka,
+		Kuningatar,
+		Kuningas
+	}
+
+	public enum Maa
+	{
+		Pata,
+		Risti,
+		Hertta,
+		Ruutu
+	}
+
+
+	public struct Kortti
+	{
+		public Arvo Arvo { get; private set; }
+		public Maa Maa { get; private set; }
+		public bool IsKnown { get; set; }
+
+		public Kortti(Arvo a, Maa m) : this()
+		{
+			Arvo = a;
+			Maa = m;
+			IsKnown = true;
+		}
+
+		public override string ToString()
+		{
+			return Maa.ToString() + " " + Arvo.ToString();
+		}
+	}
+}
+
